64c846e63ecc693b00cf6d204faa2cfcef54b08f
Coxlee Anthony Hubilla <coxleehubilla@gmail.com>
2021-03-16 14:17:05 +0800

    removed .env

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Sample Get</h3>
+    <h4>Get All</h4>
+    <pre>{{pokemon}}</pre>
+    <h4>Selected Pokemon</h4>
+    <pre>{{selected}}</pre>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      pokemon: [],
+      selected: {}
+    }
+  },
+  methods: {
+    getAllPokemon () {
+      this.$api.Pokemon.get({
+        success: (data) => {
+          console.log(data)
+          this.pokemon = JSON.stringify(data.results, null, 6)
+        },
+        fail: (error) => {
+          console.log(error)
+        }
+      })
+    },
+    getSpecificPokemon () {
+      this.$api.Pokemon.get({
+        slug: 'bulbasaur',
+        success: (data) => {
+          console.log(data)
+          this.selected = data
+        },
+        fail: (error) => {
+          console.log(error)
+        }
+      })
+    }
+  },
+  created() {
+    this.getAllPokemon()
+    this.getSpecificPokemon()
+  },
 }
 </script>
 
